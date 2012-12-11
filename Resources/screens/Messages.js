60920b90e2266d93dbf99307d25b9fefecd53535
Anthony DeBonis <anthony@troywebconsulting.com>
2012-12-11 12:58:25 -0500

    Added setActivityIndicator messages durring message gets

--- Resources/screens/Messages.js
+++ Resources/screens/Messages.js
@@ -176,9 +176,154 @@ function message_window() {
 			messageArea.test="error";
 			setActivityIndicator('');
 			
-		}	
+		}
+		/*
+			
+			
+		//Add in Scrolling support
+		var border = Ti.UI.createView({
+			backgroundColor:"#576c89",
+			height:2,
+			bottom:0
+		})
+		 
+		var tableHeader = Ti.UI.createView({
+			backgroundColor:"#e2e7ed",
+			width:320,
+			height:60
+		});
+		tableHeader.add(border);
+		
+		tableView.headerPullView = tableHeader;
+		
+		var arrow = Ti.UI.createView({
+			//backgroundImage:"../images/whiteArrow.png",
+			width:23,
+			height:60,
+			bottom:10,
+			left:20
+		});
+		 
+		var statusLabel = Ti.UI.createLabel({
+			text:"Pull to reload",
+			left:55,
+			width:200,
+			bottom:30,
+			height:"auto",
+			color:"#576c89",
+			textAlign:"center",
+			font:{fontSize:13,fontWeight:"bold"},
+			shadowColor:"#999",
+			shadowOffset:{x:0,y:1}
+		});
+		 
+		var lastUpdatedLabel = Ti.UI.createLabel({
+			text:"Last Updated: "+formatDate(),
+			left:55,
+			width:200,
+			bottom:15,
+			height:"auto",
+			color:"#576c89",
+			textAlign:"center",
+			font:{fontSize:12},
+			shadowColor:"#999",
+			shadowOffset:{x:0,y:1}
+		});
+		var actInd = Titanium.UI.createActivityIndicator({
+			left:20,
+			bottom:13,
+			width:30,
+			height:30
+		});
+		
+		function formatDate()
+		{
+			var d = new Date;
+			var datestr = d.getMonth()+'/'+d.getDate()+'/'+d.getFullYear();
+			if (d.getHours()>=12)
+			{
+		           datestr+=' '+(d.getHours()==12 ? 
+		              d.getHours() : d.getHours()-12)+':'+
+		              d.getMinutes()+' PM';
+			}
+			else
+			{
+				datestr+=' '+date.getHours()+':'+date.getMinutes()+' AM';
+			}
+			return datestr;
+		}
+		
+		//tableView.headerPullView = tableHeader;
+		log('before add ev list');
+		
+		
+		
+		tableView.addEventListener('scroll',function(e)
+		{	log('Scroll Started');
+			var offset = e.contentOffset.y;
+			if (offset <= -65.0 && !pulling)
+			{
+				var t = Ti.UI.create2DMatrix();
+				t = t.rotate(-180);
+				pulling = true;
+				arrow.animate({transform:t,duration:180});
+				statusLabel.text = "Release to refresh...";
+			}
+			else if (pulling && offset > -65.0 && offset < 0)
+			{
+				pulling = false;
+				var t = Ti.UI.create2DMatrix();
+				arrow.animate({transform:t,duration:180});
+				statusLabel.text = "Pull down to refresh...";
+			}
+		});
+		tableView.addEventListener('scrollEnd',function(e)
+		{
+			if (pulling && !reloading && e.contentOffset.y <= -65.0)
+			{
+				reloading = true;
+				pulling = false;
+				arrow.hide();
+				actInd.show();
+				statusLabel.text = "Reloading...";
+				tableView.setContentInsets({top:60},{animated:true});
+				arrow.transform=Ti.UI.create2DMatrix();
+				beginReloading();
+			}
+		});
+				
+		var pulling = false;
+		var reloading = false;
+		 
+		function beginReloading()
+		{log('begin loading');
+			// just mock out the reload
+			setTimeout(endReloading,2000);
+		}
+		 
+		function endReloading()
+		{
+			// // simulate loading 
+			// for (var c=lastRow;c<lastRow+10;c++)
+			// {
+				// tableView.appendRow({title:"Row "+c});
+			// }
+			// lastRow += 10;
+// 		 
+		log('Scroll end');
+			// when you're done, just reset
+			tableView.setContentInsets({top:0},{animated:true});
+			reloading = false;
+			lastUpdatedLabel.text = "Last Updated: "+formatDate();
+			statusLabel.text = "Pull down to refresh...";
+			actInd.hide();
+			arrow.show();
+		}		
+			
+			*/	
 	};
 		
+	
 	return win;
 };
 
