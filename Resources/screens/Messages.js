99a7d5064a4ddcb0b08b5dc32772474e586f8bc5
Anthony DeBonis <anthony@troywebconsulting.com>
2013-02-27 11:27:00 -0500

    Test #253  Fixed issue with the Message List pull down to refresh meeages Month was off by 1.
    var datestr = (date.getMonth()+1 ) + '/' + date.getDate() + '/' + date.getFullYear();

--- Resources/screens/Messages.js
+++ Resources/screens/Messages.js
@@ -440,7 +440,7 @@ function message_window() {
 
 	function formatDate() {
 		var date = new Date();
-		var datestr = date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
+		var datestr = (date.getMonth()+1 ) + '/' + date.getDate() + '/' + date.getFullYear();
 		if (date.getHours() >= 12) {
 			datestr += ' ' + (date.getHours() == 12 ? date.getHours() : date.getHours() - 12) + ':' + date.getMinutes() + ' PM';
 		} else {
