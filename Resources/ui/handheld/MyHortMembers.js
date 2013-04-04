a44fe6471be4641754dcedc6056326916a2a545c
Anthony DeBonis <anthony@troywebconsulting.com>
2013-04-04 14:40:08 -0400

    Test #303 added member list to Secondary users too > only show Non Invisible users to them

--- Resources/ui/handheld/MyHortMembers.js
+++ Resources/ui/handheld/MyHortMembers.js
@@ -1,4 +1,4 @@
-function createMyHortWindow(_myHortData, utm) {
+function createMyHortWindow(_myHortData, utm, _isOwner) {
 
 	var win = Ti.UI.createWindow({
 		backgroundColor : utm.backgroundColor,
@@ -64,105 +64,109 @@ function createMyHortWindow(_myHortData, utm) {
 		tableView.setData([]);
 		var tableData = [];
 		for (var i = 0; i < myHortMembersData.length; i++) {
-			var row = Ti.UI.createTableViewRow({
-				className : 'row',
-				row : clickName = 'row',
-				objName : 'row',
-				touchEnabled : false,
-				height : 55,
-				hasChild : false,
-				myHortMembersRowData : myHortMembersData[i]
-			});
-
-			var hView = Ti.UI.createView({
-				layout : 'composite',
-				backgroundColor : '#fff',
-				objName : 'hView'
-			});
-
-			var nickName = Ti.UI.createLabel({
-				backgroundColor : '#fff',
-				color : '#000',
-				font : {
-					fontSize : 14,
-					fontWeight : 'bold'
-				},
-				objName : 'myHortName',
-				text : myHortMembersData[i].NickName,
-				touchEnabled : false,
-				//top : 5,
-				left : 2,
-				width : 110,
-				height : 15,
-				ellipsize : false
-			});
-			hView.add(nickName);
-
-			//Member Type
-			var memberType = Ti.UI.createLabel({
-				backgroundColor : '#fff',
-				color : '#000',
-				objName : 'memberType',
-				text : myHortMembersData[i].MemberType,
-				touchEnabled : false,
-				left : 115,
-				height : 15,
-				ellipsize : false
-			});
-			hView.add(memberType);
-
-			var iconView = Ti.UI.createView({
-				layout:'horizontal'
-				,left:utm.SCREEN_WIDTH - 80			
-			});			
-			hView.add(iconView);
-			
-			if(myHortMembersData[i].HasMobile){}
-				var phoneIcon = Ti.UI.createImageView({
-						image :  '/images/phone_black.png',
-						width : myHortMembersData[i].HasMobile?14:0,
-						height : 24,
-						right:myHortMembersData[i].HasMobile?3:0,
-						top:15, 
-						visible:myHortMembersData[i].HasMobile
+			if( _isOwner || myHortMembersData[i].MemberType !=='Invisible'){
+				var row = Ti.UI.createTableViewRow({
+					className : 'row',
+					row : clickName = 'row',
+					objName : 'row',
+					touchEnabled : false,
+					height : 55,
+					hasChild : false,
+					myHortMembersRowData : myHortMembersData[i]
 				});
-				iconView.add(phoneIcon);
-			myHortMembersData[i].HasMobile	
-			
-			var emailIcon = Ti.UI.createImageView({
-					image :  '/images/email_black.png',
-					width : myHortMembersData[i].HasEmail?24:0,
-					height : 16,
-					right:myHortMembersData[i].HasEmail?3:0,
-					top:15,
-					visible:myHortMembersData[i].HasEmail
-			});
-			iconView.add(emailIcon);
-			
-			var twitterIcon = Ti.UI.createImageView({
-					image :  '/images/twitter_black.png',
-					width : myHortMembersData[i].HasTwitter?24:0,
-					height : 24,
-					right:myHortMembersData[i].HasTwitter?3:0,
-					top:15,
-					visible:myHortMembersData[i].HasTwitter
-			});
-			iconView.add(twitterIcon);
-
-			row.add(hView);
-			tableData.push(row);
+	
+				var hView = Ti.UI.createView({
+					layout : 'composite',
+					backgroundColor : '#fff',
+					objName : 'hView'
+				});
+	
+				var nickName = Ti.UI.createLabel({
+					backgroundColor : '#fff',
+					color : '#000',
+					font : {
+						fontSize : 14,
+						fontWeight : 'bold'
+					},
+					objName : 'myHortName',
+					text : myHortMembersData[i].NickName,
+					touchEnabled : false,
+					//top : 5,
+					left : 2,
+					width : 110,
+					height : 15,
+					ellipsize : false
+				});
+				hView.add(nickName);
+	
+				//Member Type
+				var memberType = Ti.UI.createLabel({
+					backgroundColor : '#fff',
+					color : '#000',
+					objName : 'memberType',
+					text : myHortMembersData[i].MemberType,
+					touchEnabled : false,
+					left : 115,
+					height : 15,
+					ellipsize : false
+				});
+				hView.add(memberType);
+	
+				var iconView = Ti.UI.createView({
+					layout:'horizontal'
+					,left:utm.SCREEN_WIDTH - 80			
+				});			
+				hView.add(iconView);
+				
+				if(myHortMembersData[i].HasMobile){}
+					var phoneIcon = Ti.UI.createImageView({
+							image :  '/images/phone_black.png',
+							width : myHortMembersData[i].HasMobile?14:0,
+							height : 24,
+							right:myHortMembersData[i].HasMobile?3:0,
+							top:15, 
+							visible:myHortMembersData[i].HasMobile
+					});
+					iconView.add(phoneIcon);
+				myHortMembersData[i].HasMobile	
+				
+				var emailIcon = Ti.UI.createImageView({
+						image :  '/images/email_black.png',
+						width : myHortMembersData[i].HasEmail?24:0,
+						height : 16,
+						right:myHortMembersData[i].HasEmail?3:0,
+						top:15,
+						visible:myHortMembersData[i].HasEmail
+				});
+				iconView.add(emailIcon);
+				
+				var twitterIcon = Ti.UI.createImageView({
+						image :  '/images/twitter_black.png',
+						width : myHortMembersData[i].HasTwitter?24:0,
+						height : 24,
+						right:myHortMembersData[i].HasTwitter?3:0,
+						top:15,
+						visible:myHortMembersData[i].HasTwitter
+				});
+				iconView.add(twitterIcon);
+	
+				row.add(hView);
+				tableData.push(row);
+			}
 		}
 
 		tableView.setData(tableData);
 
 	}
 	
-	//Add Click to Details for drilldown
-	tableView.addEventListener('click', function(e) {
-		utm.MemberDetailsWindow = require('screens/MyHortMemberDetail');
-		utm.memberDetailsWindow = new utm.MemberDetailsWindow(e.rowData.myHortMembersRowData,utm);
-		utm.navController.open(utm.memberDetailsWindow);
-	});
+	if(_isOwner){
+		//Add Click to Details for drilldown
+		tableView.addEventListener('click', function(e) {
+			utm.MemberDetailsWindow = require('screens/MyHortMemberDetail');
+			utm.memberDetailsWindow = new utm.MemberDetailsWindow(e.rowData.myHortMembersRowData,utm);
+			utm.navController.open(utm.memberDetailsWindow);
+		});
+	}
 
 	//This event handler resets the member detail after its been updated in MyHortMemberDetail	
 	Ti.App.addEventListener('app:myHortMemberDetailReload', function(e){
