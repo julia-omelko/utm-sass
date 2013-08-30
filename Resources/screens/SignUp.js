ff8a6d6b9dece4007f5caf5c5065257203373e57
Anthony DeBonis <anthony@troywebconsulting.com>
2013-08-30 12:03:50 -0400

    New Feature - 002 - Sign up on app
        #497 - Modify email confirm process
        #492 - Add Back Signup Link
        #493 - Update Signup Form
        #494 - PW Strength Meter
        #495 - Add Check Box > Rules of Use
        #496 - Add Link to Rules Of Use

--- Resources/screens/SignUp.js
+++ Resources/screens/SignUp.js
@@ -1,61 +1,106 @@
 function signUp_window(utm) {
 
 	var InputField = require('ui/common/baseui/InputField');
+	var CheckBoxField = require('ui/common/baseui/CheckBox');
+	var Header = require('ui/common/Header');
+	var PasswordMeter =require("/lib/PasswordStrengthMeter");
 	var isFormValid = false;
 	var isUserNameValid=false;
 	
-	var win = Ti.UI.createWindow({
-		layout : 'vertical',
-		backgroundColor : utm.backgroundColor,
-		barColor : utm.Android ? utm.androidBarColor : utm.barColor,
-		title : 'Sign Up',
-		backButtonTitle:'Cancel'
-	});
+	var win = new Header(utm,'Sign Up', 'Cancel');
 
 	var scrollingView = Ti.UI.createScrollView({
 		showVerticalScrollIndicator : true,
-		showHorizontalScrollIndicator : false
+		showHorizontalScrollIndicator : false,
+		width:utm.SCREEN_WIDTH
 	});
 	win.add(scrollingView);
 
 	var view = Ti.UI.createView({
-		height : 2000,
+		top:10,
+		height : 600,
+		width:utm.SCREEN_WIDTH,
 		layout : 'vertical'
 	});
 
 	scrollingView.add(view);
 
 	//-----------------User Name  ----------------------
-	var userName = new InputField(utm,'User Name', 80, '', 210, Ti.UI.KEYBOARD_DEFAULT,'',true);
+	var userName = new InputField(utm,'User Name', '40%', '', '50%', Ti.UI.KEYBOARD_DEFAULT,'',true);
+	userName.left = 5;
 	view.add(userName);
 
 	//-----------------Password  ----------------------
-	var password = new InputField(utm,'Password', 80, '', 210, Ti.UI.KEYBOARD_DEFAULT,'',true,'password');
+	var password = new InputField(utm,'Password', '40%', '', '50%', Ti.UI.KEYBOARD_DEFAULT,'',true,'password');
 	view.add(password);
-	//password.setValue('testtest1');
+	if (Ti.Platform.model === 'Simulator'  || Ti.Platform.model ===  'google_sdk') { 
+		password.setValue('Testtest1!');
+	}
 
+	var pwMeter = new PasswordMeter();
+	view.add(pwMeter);
+	
+	var spacerView = Ti.UI.createView({
+		height:5
+	});
+	view.add(spacerView);
 	//-----------------Confirm  ----------------------
-	var confirm = new InputField(utm,'Confirm Password', 80, '', 210, Ti.UI.KEYBOARD_DEFAULT,'',true, 'password');
+	var confirm = new InputField(utm,'Confirm Password', '40%', '', '50%', Ti.UI.KEYBOARD_DEFAULT,'',true, 'password');
+	confirm.left=5;
 	view.add(confirm);
-	//confirm.setValue('testtest1');
-
-	//----------Mobile # --------------------
-	var mobile = new InputField(utm,'Mobile Number', 80, '', 210, Ti.UI.KEYBOARD_DECIMAL_PAD,'',true);
-	view.add(mobile);
+	if (Ti.Platform.model === 'Simulator'  || Ti.Platform.model ===  'google_sdk') { 
+		confirm.setValue('Testtest1!');
+	}
 
 	//----------Email--------------------
-	var email = new InputField(utm,'Email', 80, '', 210, Ti.UI.KEYBOARD_EMAIL,'',true);
+	var email = new InputField(utm,'Email', '40%', '', '50%', Ti.UI.KEYBOARD_EMAIL,'',true);
+	email.left=5;
 	view.add(email);
 
-
-
+	//----------Mobile # --------------------
+	var mobile = new InputField(utm,'Mobile Number', '40%', '', '50%', Ti.UI.KEYBOARD_DECIMAL_PAD,'',false,'',12);
+	mobile.left=5;
+	view.add(mobile);
+	//todo - handle international numbers some day....
+	
+	//----------Accept ROU --------------------
+	var rouBox = Ti.UI.createView({
+		layout : 'horizontal',
+		height: '30dp',
+		top:'5dp',
+		left : '5dp'
+	});
+	view.add(rouBox);
+	
+	var rouCB = new CheckBoxField(false);
+	rouBox.add(rouCB);
+	
+	var iAcceptLabel = Ti.UI.createLabel({
+		text : "I accept YouThisMe's",
+		left: '10dp',
+		font : {
+			fontWeight : 'bold',
+			fontSize : '14dp'
+		},
+		color : '#000'
+	});
+	rouBox.add(iAcceptLabel);
+	var rouLabel = Ti.UI.createLabel({
+		text : " Rules of Use",
+		font : {
+			fontWeight : 'bold',
+			fontSize : '14dp'
+		},
+		color : 'blue'
+	});
+	rouBox.add(rouLabel);
+	
+	//----------Save Button --------------------
 	var saveButton = Ti.UI.createButton({
 		title :'Signup',
-		top : 3,
 		enabled : false
 	});
 	saveButton.addEventListener('click', function() {
-		utm.log('saveButton fired');
 		register();
 	});
 	view.add(saveButton);
@@ -69,8 +114,11 @@ function signUp_window(utm) {
 		utm.curReg.Password=password.getValue();
 		utm.curReg.ConfirmPassword=confirm.getValue();
 		utm.curReg.Email = email.getValue();
-		utm.curReg.Mobile = mobile.getValue();
+		utm.curReg.Mobile = mobile.getValue()=="" ? null:mobile.getValue();
+		utm.curReg.AcceptTerms = true;
+		utm.curReg.RegistrationMethod=utm.Android?'android':'ios';
 		
+		utm.setActivityIndicator('Sign Up in Progress...');
 		getSignUpReq.open("POST", utm.serviceUrl + "Account/Create?returnUrl=/Account/Verify");
 		getSignUpReq.setRequestHeader("Content-Type", "application/json; charset=utf-8");
 		getSignUpReq.send(JSON.stringify(utm.curReg));
@@ -80,7 +128,7 @@ function signUp_window(utm) {
 		validatesSecureCertificate : utm.validatesSecureCertificate,
 		onload : function() {
 
-			
+			utm.setActivityIndicator('');
 			var response = eval('(' + this.responseText + ')');
 			utm.signUps = response;
 
@@ -97,11 +145,13 @@ function signUp_window(utm) {
 						}else{
 							alert(error.Message);	
 						}
+					}else{
+						//Success
+						saveButton.enabled = false;
+						alert('Thank you for registering. Please check your email for a confirmation request with a link that will confirm your account. Once you click the link, your registration will be complete.'); 
+						utm.navController.close(utm.signupView);			
 					}
-					//Success
-					saveButton.enabled = false;
-					alert('Thank you for registering. Please check your email for a confirmation request with a link that will confirm your account. Once you click the link, your registration will be complete.'); 
-					utm.navController.close(utm.signupView);					
+							
 				}
 
 			} else if (this.status == 400) {
@@ -134,33 +184,59 @@ function signUp_window(utm) {
 		timeout : utm.netTimeout
 	});
 
-	userName.addEventListenerEvent('blur', function checkUserName(){		
+	userName.addEventListenerEvent('blur', function checkForm(){		
 		checkExistingUserName();
 	});
 	
-	password.addEventListenerEvent('blur', function checkUserName(){		
+	password.addEventListenerEvent('blur', function checkForm(){		
 		validateForm();
 	});
 	
-	confirm.addEventListenerEvent('blur', function checkUserName(){		
+	password.addEventListenerEvent('change', function checkForm(){		
 		validateForm();
 	});
 	
-	confirm.addEventListenerEvent('change', function checkUserName(){		
+	confirm.addEventListenerEvent('blur', function checkForm(){		
 		validateForm();
 	});
 	
-	mobile.addEventListenerEvent('blur', function checkUserName(){		
+	confirm.addEventListenerEvent('change', function checkForm(){		
+		validateForm();
+	});
+	
+	mobile.addEventListenerEvent('blur', function checkForm(){		
+		validateForm();
+	});
+	
+	mobile.addEventListenerEvent('change', function checkForm(e){		
+		/*
+		 var mobileVal = mobile.getValue();
+		 var charv = mobileVal.charAt(mobileVal.length);
+		 
+		if(mobile.getValue().charAt(charv) == '-') return;
+
+		if(mobile.getValue().length==3){
+			mobile.setValue(mobile.getValue()+'-');
+		}
+		if(mobile.getValue().length==7){
+			mobile.setValue(mobile.getValue()+'-');
+		}*/
 		validateForm();
 	});
 
-	email.addEventListenerEvent('blur', function checkUserName(){		
+	email.addEventListenerEvent('blur', function checkForm(){		
 		validateForm();
 	});
 	
-	email.addEventListenerEvent('change', function checkUserName(){		
+	email.addEventListenerEvent('change', function checkForm(){		
 		validateForm();
 	});		
+	
+	rouBox.addEventListener('click', function checkForm(){		
+		validateForm();
+	});		
+	
+	
 	
 	var checkUserNameRquest = Ti.Network.createHTTPClient({
 		validatesSecureCertificate : utm.validatesSecureCertificate,
@@ -196,6 +272,7 @@ function signUp_window(utm) {
 			&& checkConfirmPassword(password.getValue(),confirm.getValue())
 		 	&& isValidEmail(email.getValue())
 		 	&& isValidPhone(mobile.getValue())
+		 	&& rouCB.isChecked()
 		){
 			//Form is valid
 			saveButton.enabled=true;	
@@ -206,6 +283,8 @@ function signUp_window(utm) {
 			isFormValid=false;
 		}		
 		// hard to deal with this one checkExistingUserName		
+		
+		var pwStrengthCheck = pwMeter.checkPW(password.getValue());
 	}
 	
 	function checkExistingUserName(){
@@ -234,12 +313,21 @@ function signUp_window(utm) {
 	}
 	
 	function isValidPhone(_phone){
-		if(_phone && _phone.length > 0){
+		if(_phone==''){
 			return true;
-		}else{
-			return false;
-		}		
+		} else {
+			var phoneRe = /^[2-9]\d{2}[2-9]\d{2}\d{4}$/;
+		  	var digits = _phone.replace(/\D/g, "");
+		  	return (digits.match(phoneRe) !== null);
+		}
 	}
+	
+
+	rouLabel.addEventListener('click', function(e) {
+	    Ti.Platform.openURL(utm.webUrl +'/Home/RulesOfUse');
+	});
+		
+
 
 	return win;
 };
