c31e80dcf03cb7ccf81b10392c7c6f6f70be5dc6
Anthony DeBonis <anthony@troywebconsulting.com>
2013-09-11 15:32:25 -0400

    Test #487 - Pull Down Refresh

--- Resources/screens/Messages.js
+++ Resources/screens/Messages.js
@@ -3,11 +3,11 @@ function message_window(utm) {
 	var moment = require('lib/moment');
 	var navGroup = false;
 	var curMode = 'recieved';
-	var refreshButtonWidth ='40dp';
-	var refreshButtonHeight ='36dp';
+	var refreshButtonWidth = '40dp';
+	var refreshButtonHeight = '36dp';
 	var lastRow = 4;
-	
-	if(utm.iPhone || utm.iPad ){
+
+	if (utm.iPhone || utm.iPad) {
 		var win = Ti.UI.createWindow({
 			layout : 'vertical',
 			title : 'Messages',
@@ -15,8 +15,7 @@ function message_window(utm) {
 			backgroundColor : utm.backgroundColor,
 			barColor : utm.barColor
 		});
-		
-		
+
 		var tabBar = Ti.UI.iOS.createTabbedBar({
 			labels : [L('messages_recieved'), L('messages_sent')],
 			backgroundColor : utm.color,
@@ -27,15 +26,15 @@ function message_window(utm) {
 			width : 250
 		});
 		win.add(tabBar);
-	
+
 		tabBar.addEventListener('click', function(e) {
 			//get out  of edit mode.
-			if(Ti.Platform.osname == 'iphone'){
+			if (Ti.Platform.osname == 'iphone') {
 				win.setRightNavButton(edit);
 			}
 			tableView.editing = false;
 			tableView.setData([]);
-			
+
 			if (tabBar.index == 0) {
 				curMode = "recieved";
 				getMessages('recieved');
@@ -46,57 +45,58 @@ function message_window(utm) {
 				tabBar.setIndex(1);
 			}
 		});
-	}	
+	}
 
-	if(utm.Android){
-				//create the base screen and hide the Android navbar
+	if (utm.Android) {
+		//create the base screen and hide the Android navbar
 		var win = Titanium.UI.createWindow({
-		    layout : 'vertical',
-		 	backgroundColor : utm.backgroundColor,
-		    navBarHidden:true
-	    });	
-	
+			layout : 'vertical',
+			backgroundColor : utm.backgroundColor,
+			navBarHidden : true
+		});
+
 		//create a navbar for Android
 		var my_navbar = Ti.UI.createView({
-		    layout : 'horizontal',
-		    height : '44dp',
-		    width : '100%',
-		    backgroundColor : utm.androidBarColor,
-		    color : utm.backgroundColor,
-		    top:0
+			layout : 'horizontal',
+			height : '44dp',
+			width : '100%',
+			backgroundColor : utm.androidBarColor,
+			color : utm.backgroundColor,
+			top : 0
 		});
-	 
-	 	//add the navbar to the screen
+
+		//add the navbar to the screen
 		win.add(my_navbar);
-		
-		var refreshButton= Ti.UI.createButton({
-			backgroundImage:'/images/refresh.gif',
-			top :'4dp',
+
+		var refreshButton = Ti.UI.createButton({
+			backgroundImage : '/images/refresh.gif',
+			top : '4dp',
 			left : utm.SCREEN_WIDTH - 70,
 			height : refreshButtonHeight,
 			width : refreshButtonWidth
 		});
-		
+
 		my_navbar.add(refreshButton);
-		
-		refreshButton.addEventListener('click',function(){
-			refreshMessages({showProgress:true});			
+
+		refreshButton.addEventListener('click', function() {
+			refreshMessages({
+				showProgress : true
+			});
 		})
-		
 		//create a view to contain Android tab buttons
-		var tabBar = Titanium.UI.createView ({
-   			layout : 'horizontal',
-   			width : '100%',
-   			height : 55,
- 			left : 0,
-			bottom : 0  			
+		var tabBar = Titanium.UI.createView({
+			layout : 'horizontal',
+			width : '100%',
+			height : 55,
+			left : 0,
+			bottom : 0
 		});
 		win.add(tabBar);
-		
+
 		var spacer = Math.round(Ti.Platform.displayCaps.platformWidth * 0.5);
 		var btnWidth = spacer - 30;
-		var leftPos = Math.round((Ti.Platform.displayCaps.platformWidth - btnWidth * 2) * 0.5);		
-		
+		var leftPos = Math.round((Ti.Platform.displayCaps.platformWidth - btnWidth * 2) * 0.5);
+
 		var receivedButton = Ti.UI.createView({
 			top : 2,
 			width : btnWidth,
@@ -110,21 +110,23 @@ function message_window(utm) {
 		var receivedButtonLabel = Ti.UI.createLabel({
 			text : 'Received',
 			color : '#FFF',
-			font:{fontFamily:'Arial',fontSize:'14dp'}
+			font : {
+				fontFamily : 'Arial',
+				fontSize : '14dp'
+			}
 		});
-		receivedButton.add(receivedButtonLabel);		
+		receivedButton.add(receivedButtonLabel);
 		tabBar.add(receivedButton);
-		
-		receivedButton.addEventListener('click',function(e)
-		{
+
+		receivedButton.addEventListener('click', function(e) {
 			tableView.editing = false;
 			tableView.setData([]);
 			curMode = "recieved";
 			receivedButton.backgroundColor = '#336699';
 			sentButton.backgroundColor = '#6699CC';
-			getMessages('recieved');			
+			getMessages('recieved');
 		});
-		
+
 		var sentButton = Ti.UI.createView({
 			top : 2,
 			height : 50,
@@ -137,25 +139,26 @@ function message_window(utm) {
 		var sentButtonLabel = Ti.UI.createLabel({
 			text : 'Sent',
 			color : '#FFF',
-			font:{fontFamily:'Arial',fontSize:'14dp'}
+			font : {
+				fontFamily : 'Arial',
+				fontSize : '14dp'
+			}
 		});
-		sentButton.add(sentButtonLabel);		
+		sentButton.add(sentButtonLabel);
 		tabBar.add(sentButton);
-		
-		sentButton.addEventListener('click',function(e)
-		{
+
+		sentButton.addEventListener('click', function(e) {
 			tableView.editing = false;
 			tableView.setData([]);
 			curMode = "sent";
 			receivedButton.backgroundColor = '#6699CC';
-			sentButton.backgroundColor = '#336699';			
+			sentButton.backgroundColor = '#336699';
 			getMessages('sent');
-		});			
-		
+		});
+
 		//add activityIndicator to window
 		win.add(utm.activityIndicator);
 	}
-		
 
 	var tableView = Titanium.UI.createTableView({
 		left : 2,
@@ -163,20 +166,19 @@ function message_window(utm) {
 		allowsSelectionDuringEditing : true
 	});
 	win.add(tableView);
-	
+
 	//Add Click to Details for drilldown
 	tableView.addEventListener('click', function(e) {
 		var messageData = e.rowData.messageData;
 		showMessageDetail(messageData);
 	});
 
-  	function showMessageDetail(_messageData){
-  		utm.MessageDetailWindow = require('screens/MessageDetail');
-		utm.messageDetailWindow = new utm.MessageDetailWindow(_messageData, curMode,utm);
+	function showMessageDetail(_messageData) {
+		utm.MessageDetailWindow = require('screens/MessageDetail');
+		utm.messageDetailWindow = new utm.MessageDetailWindow(_messageData, curMode, utm);
 		utm.messageDetailWindow.title = 'Message';
-		utm.navController.open(utm.messageDetailWindow);  		
-  	}
-
+		utm.navController.open(utm.messageDetailWindow);
+	}
 
 	//Add Swipe event to delete messages
 	/*tableView.addEventListener('swipe', function(eventObject){
@@ -220,27 +222,26 @@ function message_window(utm) {
 
 	function callDeleteMessage(messageId, isSuperDelete) {
 		utm.log("About to delete message:" + messageId + '  isSuperDelete:' + isSuperDelete);
-		
+
 		var deleteMessagesReq = Ti.Network.createHTTPClient({
 			validatesSecureCertificate : utm.validatesSecureCertificate,
 			onload : function(e) {
 				utm.log('Message was deleted');
-	
+
 				/*var opts = {options: [L('ok_button')], title:L('messages_message_deleted')};
 				 var dialog = Ti.UI.createOptionDiautm.log(opts).show();
 				 dialog.addEventListener('click', function(e){
 				 getMessages(curMode);
 				 });*/
-				deleteMessagesReq=null;
+				deleteMessagesReq = null;
 			},
 			onerror : function(e) {
 				utm.handleError(e, this.status, this.responseText);
-				deleteMessagesReq=null;
+				deleteMessagesReq = null;
 			},
 			timeout : utm.netTimeout
 		});
-		
-		
+
 		deleteMessagesReq.open('delete', utm.serviceUrl + 'Messages/DeleteMessage/' + messageId + '?isSuperDelete=' + isSuperDelete);
 		deleteMessagesReq.setRequestHeader('Authorization-Token', utm.AuthToken);
 		deleteMessagesReq.send();
@@ -254,9 +255,9 @@ function message_window(utm) {
 	});
 
 	edit.addEventListener('click', function() {
-		if(Ti.Platform.osname == 'iphone'){
+		if (Ti.Platform.osname == 'iphone') {
 			win.setRightNavButton(cancel);
-		}	
+		}
 		tableView.editing = true;
 	});
 
@@ -265,15 +266,14 @@ function message_window(utm) {
 		style : Titanium.UI.iPhone.SystemButtonStyle.DONE
 	});
 	cancel.addEventListener('click', function() {
-		if(Ti.Platform.osname == 'iphone'){
+		if (Ti.Platform.osname == 'iphone') {
 			win.setRightNavButton(edit);
 		}
 		tableView.editing = false;
 	});
-	if(Ti.Platform.osname == 'iphone'){
+	if (Ti.Platform.osname == 'iphone') {
 		win.setRightNavButton(edit);
 	}
-		
 
 	Ti.App.addEventListener('app:showMessages', showMessageWindow);
 	function showMessageWindow() {
@@ -307,37 +307,36 @@ function message_window(utm) {
 		if (showProgress) {
 			utm.setActivityIndicator('Getting your messages...');
 		}
-		
-		
+
 		var getMessagesReq = Ti.Network.createHTTPClient({
 			validatesSecureCertificate : utm.validatesSecureCertificate,
-			
-			onload :function() {
-				var response = eval('('+this.responseText+')');
+
+			onload : function() {
+				var response = eval('(' + this.responseText + ')');
 				var tableData = [];
 				utm.setActivityIndicator('');
 				Titanium.Analytics.featureEvent('user.viewed_messages');
 				if (this.status == 200) {
-		
-					utm.log("message data returned " +  response.length + '  messages');
-		
+
+					utm.log("message data returned " + response.length + '  messages');
+
 					for (var i = 0; i < response.length; i++) {
 						var row = Ti.UI.createTableViewRow({
 							//className : 'row',
 							row : clickName = 'row',
 							objName : 'row',
 							touchEnabled : true,
-							height : utm.Android ? '55dp':55,
+							height : utm.Android ? '55dp' : 55,
 							hasChild : true,
 							messageData : response[i]
 						});
-		
+
 						var hView = Ti.UI.createView({
-							layout : 'composite', 
+							layout : 'composite',
 							backgroundColor : '#fff',
 							objName : 'hView'
 						});
-		
+
 						if (!response[i].WasRead) {
 							var unreadImage = Ti.UI.createImageView({
 								image : '/images/circle_blue.png',
@@ -348,7 +347,7 @@ function message_window(utm) {
 							});
 							hView.add(unreadImage);
 						}
-		
+
 						var fromMessage = Ti.UI.createLabel({
 							backgroundColor : '#fff',
 							color : '#000',
@@ -362,11 +361,11 @@ function message_window(utm) {
 							top : 2,
 							left : 17,
 							width : utm.SCREEN_WIDTH - 100,
-							height : utm.Android ? '15dp':15,
+							height : utm.Android ? '15dp' : 15,
 							ellipsize : true
 						});
 						hView.add(fromMessage);
-		
+
 						var utmMessage = Ti.UI.createLabel({
 							backgroundColor : '#fff',
 							color : '#666',
@@ -378,11 +377,11 @@ function message_window(utm) {
 							touchEnabled : true,
 							top : 30,
 							left : 15,
-							height : utm.Android ? '20dp':'20',
+							height : utm.Android ? '20dp' : '20',
 							width : '100%'
 						});
 						hView.add(utmMessage);
-		
+
 						var timeLabel = Ti.UI.createLabel({
 							backgroundColor : '#fff',
 							color : '#0066ff',
@@ -391,7 +390,7 @@ function message_window(utm) {
 							},
 							objName : 'timeLabel',
 							//text: String.formatDate(new Date(response[i].DateSent,'short')),
-		
+
 							text : getDateTimeFormat(response[i].DateSent), // moment(response[i].DateSent).fromNow(),
 							//text: easyFormat(new Date(response[i].DateSent)),//response[i].DateSent,
 							touchEnabled : true,
@@ -400,26 +399,26 @@ function message_window(utm) {
 							width : 'auto'
 						});
 						hView.add(timeLabel);
-		
+
 						row.add(hView);
-						tableData.push(row);	
+						tableData.push(row);
 					}
-		
+
 					tableView.setData(tableData);
-		
+
 				} else if (this.status == 400) {
-		
+
 					utm.recordError("Error:" + this.responseText);
-		
+
 				} else {
 					utm.recordError("error");
 				}
-				getMessagesReq=null;
-			}, 
-			
+				getMessagesReq = null;
+			},
+
 			onerror : function(e) {
 				utm.handleError(e, this.status, this.responseText);
-				getMessagesReq=null;
+				getMessagesReq = null;
 			},
 			timeout : utm.netTimeout
 		});
@@ -438,35 +437,29 @@ function message_window(utm) {
 		//	}
 	}
 
-	
-
- 
-	
-
-	
 	//RE #260 - Mobile Ti App: ver 0.28 Beta: Latency, can briefly see messages from previously logged in user
 	Ti.App.addEventListener('app:loginSuccess', handleLoginSuccess);
 	function handleLoginSuccess(event) {
-		tableData = [];	
+		tableData = [];
 		tableView.setData(tableData);
-	}	
+	}
 
 	// #############################  Scroll Refresh Start #############################
-	if(! utm.Android){ 
+	if (!utm.Android) {
 		var border = Ti.UI.createView({
 			backgroundColor : "#576c89",
 			height : 2,
 			bottom : 0
 		});
-	
+
 		var tableHeader = Ti.UI.createView({
 			backgroundColor : "#e2e7ed",
 			width : 320,
 			height : 60
 		});
-	
+
 		tableHeader.add(border);
-	
+
 		var arrow = Ti.UI.createView({
 			backgroundImage : "/images/whiteArrow.png",
 			width : 23,
@@ -474,7 +467,7 @@ function message_window(utm) {
 			bottom : 10,
 			left : 20
 		});
-	
+
 		var statusLabel = Ti.UI.createLabel({
 			text : "Pull to reload",
 			left : 55,
@@ -493,7 +486,7 @@ function message_window(utm) {
 				y : 1
 			}
 		});
-	
+
 		var lastUpdatedLabel = Ti.UI.createLabel({
 			text : "Last Updated: " + formatDate(),
 			left : 55,
@@ -511,36 +504,36 @@ function message_window(utm) {
 				y : 1
 			}
 		});
-	
+
 		var actInd = Titanium.UI.createActivityIndicator({
 			left : 20,
 			bottom : 13,
 			width : 30,
 			height : 30
 		});
-	
+
 		tableHeader.add(arrow);
 		tableHeader.add(statusLabel);
 		tableHeader.add(lastUpdatedLabel);
 		tableHeader.add(actInd);
-	
+
 		tableView.headerPullView = tableHeader;
-	
+
 		var pulling = false;
 		var reloading = false;
-	
+
 		function beginReloading() {
 			// just mock out the reload
 			setTimeout(endReloading, 500);
 		}
-	
-		function endReloading() {		
-			var data='';
+
+		function endReloading() {
+			var data = '';
 			//call to get the data
 			getMessages(curMode, true);
-	
+
 			lastRow += 10;
-	
+
 			// when done reset
 			tableView.setContentInsets({
 				top : 0
@@ -555,8 +548,7 @@ function message_window(utm) {
 		}
 
 
-
-		tableView.addEventListener('scroll', function(e) {	
+		tableView.addEventListener('scroll', function(e) {
 			var offset = e.contentOffset.y;
 			if (offset <= -65.0 && !pulling && !reloading) {
 				var t = Ti.UI.create2DMatrix();
@@ -577,12 +569,12 @@ function message_window(utm) {
 				statusLabel.text = "Pull down to refresh...";
 			}
 		});
-	
+
 		var event1 = 'dragEnd';
 		if (Ti.version >= '3.0.0') {
 			event1 = 'dragend';
 		}
-	
+
 		tableView.addEventListener(event1, function(e) {
 			if (pulling && !reloading) {
 				reloading = true;
@@ -600,20 +592,18 @@ function message_window(utm) {
 			}
 		});
 	}
-	
-	Number.prototype.pad = function (len) {
- 	   return (new Array(len+1).join("0") + this).slice(-len);
+
+	Number.prototype.pad = function(len) {
+		return (new Array(len + 1).join("0") + this).slice(-len);
 	}
-	
-	function pad(val){
-		val = val < 10 ? '0' +val: '' +val;
+	function pad(val) {
+		val = val < 10 ? '0' + val : '' + val;
 		return val;
 	}
-	
 
 	function formatDate() {
 		var date = new Date();
-		var datestr = (date.getMonth()+1 ) + '/' + date.getDate() + '/' + date.getFullYear();
+		var datestr = (date.getMonth() + 1 ) + '/' + date.getDate() + '/' + date.getFullYear();
 		if (date.getHours() >= 12) {
 			datestr += ' ' + (date.getHours() == 12 ? date.getHours() : date.getHours() - 12) + ':' + pad(date.getMinutes()) + ' PM';
 		} else {
@@ -623,17 +613,15 @@ function message_window(utm) {
 	}
 
 	// #############################  Scroll Refresh End #############################
-	
+
 	win.addEventListener('blur', function() {
-		if(Ti.Platform.osname == 'iphone'){
+		if (Ti.Platform.osname == 'iphone') {
 			win.setRightNavButton(edit);
-		}   		
+		}
 		tableView.editing = false;
 	});
-
-
 
 	return win;
 };
 
-module.exports = message_window;
+module.exports = message_window; 
