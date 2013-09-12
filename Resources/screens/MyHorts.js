2b6070d9f879434cb92585a4b5b417f9a46ba73c
Anthony DeBonis <anthony@troywebconsulting.com>
2013-09-12 10:58:59 -0400

    -Refactored screen to use common Header.js

--- Resources/screens/MyHorts.js
+++ Resources/screens/MyHorts.js
@@ -1,53 +1,31 @@
 var MyHorts_window = function(utm) {
 
 	var CreateMyHortWindow = require('ui/handheld/CreateMyHort');
+	var Header = require('ui/common/Header');
 
-	if(utm.iPhone || utm.iPad ){
-		var myHortsWindow = Titanium.UI.createWindow({
-			layout : 'vertical',
-			title : 'MyHorts',
-			backgroundColor : utm.backgroundColor,
-			backButtonTitle : L('button_back'),
-			barColor : utm.barColor
-		});
-	}
-	if(utm.Android){
-		//create the base screen and hide the Android navbar
-		var myHortsWindow = Titanium.UI.createWindow({
-		    layout : 'vertical',
-		 	backgroundColor : utm.backgroundColor,
-		    navBarHidden:true
-	    });
+	var myHortsWindow = new Header(utm, 'MyHorts', L('button_back'));
 
- 		//create a navbar for Android
-		var my_navbar = Ti.UI.createLabel({
-		    height : 50,
-		    width : '100%',
-		    backgroundColor : utm.androidBarColor,
-		    text:'MyHorts',
-		    color : utm.backgroundColor,
-		    font:{fontSize:utm.androidTitleFontSize,fontWeight:utm.androidTitleFontWeight},
-		    textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
-		    top:0
-		});
- 
- 		//add the navbar to the screen
-		myHortsWindow.add(my_navbar);
-		
-		//add activityIndicator to window
-		myHortsWindow.add(utm.activityIndicator)
-		
+	var deleteButton = Titanium.UI.createButton({
+		title : 'Delete'
+	});
+
+	if (utm.iPhone || utm.iPad) {
+		myHortsWindow.setRightNavButton(deleteButton);
 	}
-	
+
+
+	//add activityIndicator to window
+	myHortsWindow.add(utm.activityIndicator)
+
 	var createButton = Ti.UI.createButton({
 		title : 'Create a New MyHort',
-		top :5
+		top : 5
 	});
 	myHortsWindow.add(createButton);
 
 	createButton.addEventListener('click', function() {
 		utm.createMyHortWindow = new CreateMyHortWindow(utm);
-		
+
 		utm.createMyHortWindow.open({
 			modal : true,
 			modalTransitionStyle : Ti.UI.iPhone.MODAL_TRANSITION_STYLE_PARTIAL_CURL,
@@ -68,15 +46,11 @@ var MyHorts_window = function(utm) {
 		loadMyHorts();
 	});
 
-	var edit = Titanium.UI.createButton({
-		title : 'Delete'
-	});
-
-	edit.addEventListener('click', function() {
-		if(utm.iPhone || utm.iPad ){
+	deleteButton.addEventListener('click', function() {
+		if (utm.iPhone || utm.iPad) {
 			myHortsWindow.setRightNavButton(cancel);
-		}	
-		tableView.editing = true;
+			tableView.editing = true;
+		}		
 	});
 
 	var cancel = Titanium.UI.createButton({
@@ -84,15 +58,15 @@ var MyHorts_window = function(utm) {
 		style : Titanium.UI.iPhone.SystemButtonStyle.DONE
 	});
 	cancel.addEventListener('click', function() {
-		if(utm.iPhone || utm.iPad ){
-			myHortsWindow.setRightNavButton(edit);
-		}	
+		if (utm.iPhone || utm.iPad) {
+			myHortsWindow.setRightNavButton(deleteButton);
+		}
 		tableView.editing = false;
 	});
-	if(utm.iPhone || utm.iPad ){
-		myHortsWindow.setRightNavButton(edit);
+	if (utm.iPhone || utm.iPad) {
+		myHortsWindow.setRightNavButton(deleteButton);
 	}
-	
+
 	function loadMyHorts() {
 		getMyHortsReq.open("GET", utm.serviceUrl + "MyHort?$orderby=FriendlyName");
 		getMyHortsReq.setRequestHeader('Authorization-Token', utm.AuthToken);
@@ -105,14 +79,14 @@ var MyHorts_window = function(utm) {
 			utm.handleError(e, this.status, this.responseText);
 		},
 		onload : function(e) {
-			var response = eval('('+this.responseText+')');
+			var response = eval('(' + this.responseText + ')');
 			utm.setActivityIndicator('');
 			Titanium.Analytics.featureEvent('user.viewed_myHorts');
 			if (this.status == 200) {
 
 				utm.log("MyHort data returned " + response.length + '  myHorts returned');
 				populateTable(response);
-				utm.User.MyHorts=response;
+				utm.User.MyHorts = response;
 			}
 		},
 		timeout : utm.netTimeout
@@ -137,17 +111,16 @@ var MyHorts_window = function(utm) {
 				backgroundColor : '#fff',
 				objName : 'hView'
 			});
-			
-			
+
 			var icon = Ti.UI.createImageView({
-					image : myHortData[i].IsOwner ? '/images/ownerIcon.png' : '/images/memberIcon.png',
-					width : 20,
-					height : 20,
-					left:3,
-					top:15
+				image : myHortData[i].IsOwner ? '/images/ownerIcon.png' : '/images/memberIcon.png',
+				width : 20,
+				height : 20,
+				left : 3,
+				top : 15
 			});
 			hView.add(icon);
-			
+
 			var myHortName = Ti.UI.createLabel({
 				backgroundColor : '#fff',
 				color : '#000',
@@ -177,7 +150,7 @@ var MyHorts_window = function(utm) {
 	tableView.addEventListener('click', function(e) {
 		var myHortData = e.rowData.myHortData;
 		utm.MyHortDetailWindow = require('screens/MyHortDetail');
-		utm.myHortDetailWindow = new utm.MyHortDetailWindow(myHortData,utm,myHortData.IsOwner);
+		utm.myHortDetailWindow = new utm.MyHortDetailWindow(myHortData, utm, myHortData.IsOwner);
 		utm.myHortDetailWindow.title = myHortData.FriendlyName;
 		utm.navController.open(utm.myHortDetailWindow);
 
@@ -185,20 +158,20 @@ var MyHorts_window = function(utm) {
 
 	tableView.addEventListener('delete', function(e) {
 		var s = e.section;
-		
-		if(!e.rowData.myHortData.IsOwner){
+
+		if (!e.rowData.myHortData.IsOwner) {
 			populateTable(utm.User.MyHorts);
 			alert('You can only delete MyHorts that you own.');
 			return;
 		}
-		
+
 		confirmDeleteMyHort(e.rowData.myHortData.MyHortId, false)
 	});
 
 	function confirmDeleteMyHort(myHortId) {
 		var dialog = Ti.UI.createAlertDialog({
 			cancel : 1,
-			buttonNames : ['Delete', L('cancel')],
+			buttonNames : ['Yes', L('cancel')],
 			message : 'Delete this MyHort will delete all your information in this MyHort - do you want to continue? ',
 			title : 'Confirm Delete',
 			myHortId : myHortId
@@ -206,7 +179,7 @@ var MyHorts_window = function(utm) {
 		dialog.addEventListener('click', function(e) {
 			if (e.index === 0) {
 				deleteMyHort(myHortId);
-			}else{
+			} else {
 				populateTable(utm.User.MyHorts);
 			}
 		});
@@ -228,7 +201,7 @@ var MyHorts_window = function(utm) {
 		validatesSecureCertificate : utm.validatesSecureCertificate,
 		onload : function() {
 			utm.setActivityIndicator('');
-						
+
 			//loadMyHorts();
 		},
 		onerror : function() {
@@ -236,21 +209,21 @@ var MyHorts_window = function(utm) {
 			utm.log('error');
 		}
 	});
-	
-	function removeFromUsersList(myHortId){
-		for(i=0; i< utm.User.MyHorts.length; i++ ){
+
+	function removeFromUsersList(myHortId) {
+		for ( i = 0; i < utm.User.MyHorts.length; i++) {
 			var mh = utm.User.MyHorts[i];
-			if(mh.MyHortId= myHortId){
-				utm.User.MyHorts.splice(i,1);
+			if (mh.MyHortId = myHortId) {
+				utm.User.MyHorts.splice(i, 1);
 				break;
 			}
 		}
 	}
-	
+
 
 	myHortsWindow.addEventListener('blur', function() {
-		if(utm.iPhone || utm.iPad){
-			myHortsWindow.setRightNavButton(edit);
+		if (utm.iPhone || utm.iPad) {
+			myHortsWindow.setRightNavButton(deleteButton);
 		}
 		tableView.editing = false;
 	});
@@ -258,10 +231,10 @@ var MyHorts_window = function(utm) {
 	Ti.App.addEventListener('app:loadMyHorts', function() {
 		loadMyHorts();
 	});
-	
+
 	//Added to clear out the myhort list in case someone re-logs in as another user
-	Ti.App.addEventListener('app:loginSuccess', function(){
-		tableView.setData([]);		
+	Ti.App.addEventListener('app:loginSuccess', function() {
+		tableView.setData([]);
 	});
 
 	return myHortsWindow;
