a4727e543453bc689de37fdffebfddfdfe939c9e
markkedik <mark.kedik@troyweb.com>
2014-03-13 00:29:05 -0400

    re#824 layout changes for iPad orientation change

--- Resources/ui/common/Compose.js
+++ Resources/ui/common/Compose.js
@@ -25,7 +25,12 @@ var ComposeWin = function(_tabGroup,_selectedContacts,_mode,_messageData) {
 		top: utm.viewableTop,
 		contentHeight: Ti.Platform.displayCaps.platformHeight*2
 	});
-	self.add(scrollingView);
+	self.add(scrollingView);	
+	
+	var adjustscrollingView = function() {
+		scrollingView.height = utm.viewableArea - ((40*utm.sizeMultiplier)+20);
+	};
+	Ti.App.addEventListener('orientdisplay', adjustscrollingView);
 	
 	var topView = Ti.UI.createView({
 		width: '100%',
@@ -42,9 +47,10 @@ var ComposeWin = function(_tabGroup,_selectedContacts,_mode,_messageData) {
 	});
 	topView.add(leftView);
 	
-	Ti.App.addEventListener('orientdisplay', function(evt) {
+	var adjustleftView = function() {
 		leftView.width = Ti.Platform.displayCaps.platformWidth-(80*utm.sizeMultiplier)-35;
-	});
+	};
+	Ti.App.addEventListener('orientdisplay', adjustleftView);
 	
 	var sendHeader = Ti.UI.createLabel({
 		text: _mode + ' to:',
@@ -70,9 +76,10 @@ var ComposeWin = function(_tabGroup,_selectedContacts,_mode,_messageData) {
 	});
 	leftView.add(sendLabel);
 	
-	Ti.App.addEventListener('orientdisplay', function(evt) {
+	var adjustsendLabel = function() {
 		sendLabel.width = Ti.Platform.displayCaps.platformWidth-50-(80*utm.sizeMultiplier)-10;
-	});
+	};
+	Ti.App.addEventListener('orientdisplay', adjustsendLabel);
 	
 	var messageHeader = Ti.UI.createLabel({
 		text: 'Message:',
@@ -109,12 +116,14 @@ var ComposeWin = function(_tabGroup,_selectedContacts,_mode,_messageData) {
 	var camera = new Camera(previewImage);
 	
 
-	
-	
-	var textAreaHeight = ((utm.Android) ? 80*utm.sizeMultiplier : (utm.viewableArea - 125 + 50 - ((40*utm.sizeMultiplier)+20) - utm.keyboardHeight));
-	if (textAreaHeight < 80) {
-		textAreaHeight = 80;
+	var textAreaHeight;
+	function calctextAreaHeight() {
+		textAreaHeight = ((utm.Android) ? 80*utm.sizeMultiplier : (utm.viewableArea - 125 + 50 - ((40*utm.sizeMultiplier)+20) - utm.keyboardHeight));
+		if (textAreaHeight < 80) {
+			textAreaHeight = 80;
+		}
 	}
+	calctextAreaHeight();
 	
 	/*var messageFieldMaxHeight = Ti.UI.createView({
 		width: Ti.UI.SIZE,
@@ -138,9 +147,12 @@ var ComposeWin = function(_tabGroup,_selectedContacts,_mode,_messageData) {
 		font: {fontFamily: utm.fontFamily, fontSize: '16dp'},
 	});
 	
-	Ti.App.addEventListener('orientdisplay', function(evt) {
+	var adjustmessageField = function() {
 		messageField.width = Ti.Platform.displayCaps.platformWidth-50;
-	});
+		calctextAreaHeight();
+		messageField.height = textAreaHeight;
+	};
+	Ti.App.addEventListener('orientdisplay', adjustmessageField);
 	
 	var messageFocued = Ti.UI.createView({
 		width: Ti.UI.FILL,
@@ -149,24 +161,42 @@ var ComposeWin = function(_tabGroup,_selectedContacts,_mode,_messageData) {
 		bottom: 0
 	});
 	
-	Ti.App.addEventListener('orientdisplay', function(evt) {
+	var adjustmessageFocued = function() {
 		messageFocued.width = Ti.UI.FILL;
-	});	
+	};
+	Ti.App.addEventListener('orientdisplay', adjustmessageFocued);
+	
+	var messageFieldHasFocus = false;
 	
 	messageField.addEventListener('focus', function() {
 		messageField.add(messageFocued);
+		messageFieldHasFocus = true;
 		if ((utm.iPhone || utm.iPad) && utm.keyboardHeight !== 0) {
 			previewBtn.setBottom(10+utm.keyboardHeight-50);
 			scrollingView.setHeight(utm.viewableArea-60-utm.keyboardHeight+50);
 		}
 	});
+	
 	messageField.addEventListener('blur', function() { 
 		messageField.remove(messageFocued);
+		messageFieldHasFocus = false;
 		if (utm.iPhone || utm.iPad) {
 			previewBtn.setBottom(10);
 			scrollingView.setHeight(utm.viewableArea-60);
 		}
 	});
+	
+	var adjustmessageFocusBlur = function() {
+		if ((utm.iPhone || utm.iPad) && utm.keyboardHeight !== 0 && messageFieldHasFocus) {
+			previewBtn.setBottom(10+utm.keyboardHeight-50);
+			scrollingView.setHeight(utm.viewableArea-60-utm.keyboardHeight+50);
+		} else if ((utm.iPhone || utm.iPad) && !messageFieldHasFocus) {
+			previewBtn.setBottom(10);
+			scrollingView.setHeight(utm.viewableArea-60);
+		}
+	};
+	Ti.App.addEventListener('orientdisplay', adjustmessageFocusBlur);	
+	
 	messageField.addEventListener('change', function() { 
 		self.monitorGuid();
 		/*if (messageField.rect.height > maxHeight) {
@@ -213,7 +243,17 @@ var ComposeWin = function(_tabGroup,_selectedContacts,_mode,_messageData) {
 		var previewWin = new PreviewWin(_tabGroup,message);
 		utm.winStack.push(previewWin);
 		_tabGroup.getActiveTab().open(previewWin);
-	});	
+	});
+	
+	self.addEventListener('close', function(e) {
+		Ti.App.removeEventListener('orientdisplay', adjustmessageFocued);
+		Ti.App.removeEventListener('orientdisplay', adjustmessageField);
+		Ti.App.removeEventListener('orientdisplay', adjustsendLabel);
+		Ti.App.removeEventListener('orientdisplay', adjustleftView);
+		Ti.App.removeEventListener('orientdisplay', adjustscrollingView);
+		Ti.App.removeEventListener('orientdisplay', adjustmessageFocusBlur);
+	});
+
 	
 	return self;
 };
